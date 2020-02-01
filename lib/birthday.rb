5969d07729f16df4463d32c498838a7cf765e820
Mark Drehmann <markdrehmann@gmail.com>
2020-02-01 17:11:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,9 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!" if age < 13
+  end
+end
